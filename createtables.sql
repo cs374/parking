6d8b0ce8e9ee39629b0cf5a8b0bf5ef72f691650
Alex Tran <33785145+alextranjmu@users.noreply.github.com>
2022-02-21 18:35:38 -0500

    Fixed createtables and updated csv file names so that they match

--- createtables.sql
+++ createtables.sql
@@ -2,20 +2,23 @@ DROP TABLE IF EXISTS garage;
 
 CREATE TABLE garage (
 	zone_id integer NOT NULL,
-	num_spaces integer NOT NULL,
 	deck text NOT NULL,
-	used_by text NOT NULL
+	zone text NOT NULL,
+	spaces integer NOT NULL
 );
+ALTER TABLE garage OWNER TO parking;
 
 COMMENT ON TABLE garage IS 'Parking garages at JMU';
 
 DROP TABLE IF EXISTS timestamps;
 
 CREATE TABLE timestamps (
+	time_stamp text NOT NULL,
 	zone_id integer NOT NULL,
-	time text NOT NULL,
 	occupied integer NOT NULL,
 	visitor integer NOT NULL
 );
 
-COMMENT ON TABLE timestamps IS 'Timestamps for parking at JMU'
+ALTER TABLE timestamps OWNER TO parking;
+
+COMMENT ON TABLE timestamps IS 'Timestamps for parking at JMU';
