827f454f511040f922c935de8a957b0ae4fad6ce
Alex Tran <anh.alex.tran@gmail.com>
2022-02-23 14:38:13 -0500

    added constraints

--- alter.sql
+++ alter.sql
@@ -1,12 +1,11 @@
 ALTER TABLE garage ADD PRIMARY KEY (zone_id);
 
-ALTER TABLE timestamps ADD PRIMARY KEY (zone_id)
+ALTER TABLE timestamps ADD PRIMARY KEY (zone_id, time_stamp);
 ALTER TABLE timestamps ADD FOREIGN KEY (zone_id) REFERENCES garage;
-ALTER TABLE timestamps ADD PRIMARY KEY (timestamp)
 
 
 ALTER TABLE classes ADD FOREIGN KEY (zone_id) REFERENCES garage;
-ALTER TABLE classes ADD PRIMARY KEY (start_time);
-ALTER TABLE classes ADD PRIMARY KEY (end_time);
+ALTER TABLE classes ADD PRIMARY KEY (zone_id, start_time, end_time, day);
+--ALTER TABLE classes ADD PRIMARY KEY (end_time);
 
 
