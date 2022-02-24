7e5f85c9f6e1ae06bd7a333e6baf901f173bc95c
baileyga <baileyga@dukes.jmu.edu>
2022-02-24 10:03:42 -0500

    added comments, not to add echo statements

--- schema/copy.sh
+++ schema/copy.sh
@@ -1,3 +1,11 @@
-psql -c "\copy garage FROM garage.csv WITH CSV HEADER" parking
+# NOTE: \copy imports from a local file
+#       COPY imports from another database
+# include echo statements
+
+psql -c "\copy garage FROM garage.csv WITH CSV HEADER" parkingS
+
+
 psql -c "\copy occupancy FROM occupancy.csv WITH CSV HEADER" parking
+
+
 psql -c "\copy classes FROM classes.csv WITH CSV HEADER" parking
