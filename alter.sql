e62e79fb4e45fc574a3ba7185257bc909c475400
baileyga <baileyga@dukes.jmu.edu>
2022-02-23 19:31:23 -0500

    testing push

--- alter.sql
+++ alter.sql
@@ -8,4 +8,6 @@ ALTER TABLE classes ADD FOREIGN KEY (zone_id) REFERENCES garage;
 ALTER TABLE classes ADD PRIMARY KEY (zone_id, start_time, end_time, day);
 --ALTER TABLE classes ADD PRIMARY KEY (end_time);
 
+--hi
+
 
