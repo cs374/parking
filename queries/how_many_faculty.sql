5ca6e18663612dd70f4efc6a844678aa33b36054
baileyga <baileyga@dukes.jmu.edu>
2022-03-11 00:45:14 -0500

    updated sql code (changed input to being just the timestamp) and importing html file of basic pie chart for one timestamp

--- queries/how_many_faculty.sql
+++ queries/how_many_faculty.sql
@@ -1,8 +1,8 @@
 -- looks at how many spaces are occupied in faculty lots by day
 -- and shows the number of available spots in the respective faculty lot
-DROP FUNCTION IF EXISTS how_many_faculty(g_zone integer, day text);
+DROP FUNCTION IF EXISTS how_many_faculty(time_of_day timestamp);
 
-CREATE FUNCTION how_many_faculty(g_zone integer, day text)
+CREATE FUNCTION how_many_faculty(time_of_day timestamp)
 RETURNS TABLE(deck text, time_stamp text, occupied_spaces integer, available_spaces integer) AS $$
 
  SELECT garage.deck, o.time_stamp, o.occupied, CAST(SUM(garage.spaces - o.occupied) AS integer) AS available 
@@ -17,4 +17,4 @@ ORDER BY time_stamp, deck
 
 $$ LANGUAGE SQL STABLE STRICT;
 
-ALTER FUNCTION how_many_faculty(g_zone integer, day text) OWNER TO parking;
+ALTER FUNCTION how_many_faculty(time_of_day timestamp) OWNER TO parking;
