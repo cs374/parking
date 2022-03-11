45f380d8a8ba09e1dfa0aa9bbecf15a100c85a9b
baileyga <baileyga@dukes.jmu.edu>
2022-03-10 23:52:55 -0500

    query now also shows available sports in each faculty lot as well

--- schema/how_many_faculty.sql
+++ schema/how_many_faculty.sql
@@ -1,16 +1,19 @@
+-- looks at how many spaces are occupied in faculty lots by day
+-- and shows the number of available spots in the respective faculty lot
 DROP FUNCTION IF EXISTS how_many_faculty(g_zone integer, day text);
 
 CREATE FUNCTION how_many_faculty(g_zone integer, day text)
-RETURNS TABLE(deck text, time_stamp text, occupied_spaces integer) AS $$
+RETURNS TABLE(deck text, time_stamp text, occupied_spaces integer, available_spaces integer) AS $$
 
-  SELECT deck, time_stamp, occupied
-  FROM garage
+ SELECT garage.deck, o.time_stamp, o.occupied, CAST(SUM(garage.spaces - o.occupied) AS integer) AS available 
+ FROM garage
     JOIN occupancy AS o ON garage.zone_id = o.zone_id
   WHERE garage.zone_id = 2
 	OR garage.zone_id = 6
 	OR garage.zone_id = 12
 	OR garage.zone_id = 27
-	ORDER BY time_stamp, deck
+GROUP BY garage.deck, o.time_stamp, o.occupied
+ORDER BY time_stamp, deck
 
 $$ LANGUAGE SQL STABLE STRICT;
 
