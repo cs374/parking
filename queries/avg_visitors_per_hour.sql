262217b4d08df69ca05b32d27afbc106adba3f09
catherineBaker6 <catherineBaker6@users.noreply.github.com>
2022-03-13 15:17:10 -0400

    Update avg_visitors_per_hour.sql
    Updated return data type.

--- queries/avg_visitors_per_hour.sql
+++ queries/avg_visitors_per_hour.sql
@@ -2,7 +2,7 @@
 DROP FUNCTION IF EXISTS avg_visitors_per_hour(g_name text);
 
 CREATE FUNCTION avg_visitors_per_hour(g_name text)
-RETURNS TABLE(deck text, hour text, avg_visitors numeric) AS $$
+RETURNS TABLE(deck text, hour text, avg_visitors bigint) AS $$
 
 
 SELECT DISTINCT deck, SUBSTRING(o.time_stamp,12,2), 
@@ -11,7 +11,7 @@ FROM garage AS g
 	JOIN occupancy AS o ON g.zone_id = o.zone_id
 WHERE g.deck = $1
 GROUP BY deck, SUBSTRING(o.time_stamp,12,2)
-ORDER BY SUBSTRING(o.time_stamp,12, 2);
+ORDER BY SUBSTRING(o.time_stamp,12, 2)
 	
 $$ LANGUAGE SQL STABLE STRICT;
 ALTER FUNCTION avg_visitors_per_hour(g_name text) OWNER TO parking;
