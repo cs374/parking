54a351d2da4ab175e8f8f149a298abd74046a7c7
Jacquelyn Hendricks <hendrijn@dukes.jmu.edu>
2022-03-26 14:29:40 -0400

    jackies functions updated w date/time

--- queries/garage_day_stats.sql
+++ queries/garage_day_stats.sql
@@ -2,18 +2,18 @@
 -- Gets the daily garage occupancy based on the garage and day.
 --
 
-DROP FUNCTION IF EXISTS garage_daily_stats(g_zone integer, day text);
+DROP FUNCTION IF EXISTS garage_daily_stats(g_zone integer, day date);
 
-CREATE FUNCTION garage_daily_stats(g_zone integer, day text)
-RETURNS TABLE(time_stamp text, occ_spaces integer) AS $$
+CREATE FUNCTION garage_daily_stats(g_zone integer, day date)
+RETURNS TABLE(time_stamp timestamp, occ_spaces integer) AS $$
 
   SELECT time_stamp, occupied
   FROM garage
     JOIN occupancy AS o ON garage.zone_id = o.zone_id
   WHERE garage.zone_id = $1
-    AND o.time_stamp LIKE CONCAT('%', $2, '%')
+    AND DATE(o.time_stamp) = day
 $$ LANGUAGE SQL STABLE STRICT;
 
-ALTER FUNCTION garage_daily_stats(g_zone integer, day text) OWNER TO parking;
+ALTER FUNCTION garage_daily_stats(g_zone integer, day date) OWNER TO parking;
 
 
--- queries/how_many_stucour.sql
+++ queries/how_many_stucour.sql
@@ -2,13 +2,11 @@
 -- Determines how many estimated students and courses were on campus at a given term and day.
 -- Note: this doesn't consider that one student may be in sequential courses at one time.
 --
--- day and start_time won't be text once we figure out the timestamp issues
---
 
 DROP FUNCTION IF EXISTS how_many_stucour(term integer, day text);
 
 CREATE FUNCTION how_many_stucour(term integer, day text)
-RETURNS TABLE(beg_time text, num_students bigint, num_courses bigint) AS $$
+RETURNS TABLE(beg_time time, num_students bigint, num_courses bigint) AS $$
 
     SELECT beg_time, sum(enrolled) AS num_students, count(*) AS num_courses
     FROM enrollment
