e4d93fdff918b51d9577d51d8529b53ee82f0008
Alex Tran <anh.alex.tran@gmail.com>
2022-03-27 22:38:06 -0400

    Changed my query to match the new timestamp data type

--- queries/class_time_garage_avail.sql
+++ queries/class_time_garage_avail.sql
@@ -1,19 +1,35 @@
 --This will show you the number of parking spots available at 
 --each garage at a given time (which is the parameter). 
-DROP FUNCTION IF EXISTS class_time_garage_avail(start_time text);
+DROP FUNCTION IF EXISTS class_time_garage_avail(start_time time);
 
-CREATE FUNCTION class_time_garage_avail(start_time text) 
+CREATE FUNCTION class_time_garage_avail(start_time time) 
 RETURNS TABLE(deck text, available_spots integer) AS $$
 
 SELECT ga.deck, CAST(AVG(ga.spaces - o.occupied) AS integer) AS avail 
 FROM occupancy AS o
 	JOIN garage AS ga ON ga.zone_id = o.zone_id
 	JOIN enrollment AS e ON 1 = 1
-WHERE o.time_stamp LIKE CONCAT('% ', SUBSTRING($1, 1, 2), ':', CAST((CAST(SUBSTRING($1, 4, 5) AS INT) + 1) AS VARCHAR(20)))
-AND   e.beg_time LIKE CONCAT('%',$1, '%')
+
+WHERE CASE WHEN (CAST(start_time AS time) < '00:10') OR (CAST(start_time AS time) > '11:50') THEN
+
+	CAST(o.time_stamp AS time) = '00:15'
+	
+ELSE
+	CAST(o.time_stamp AS time) < (CAST(start_time AS time) + interval '10 minutes')
+	AND CAST(o.time_stamp AS time) > (CAST(start_time AS time) - interval '10 minutes')
+	
+END
+
+AND EXTRACT(MONTH FROM o.time_stamp) < 9
+AND EXTRACT(MONTH FROM o.time_stamp) > 5
+
+-- AND CAST(e.beg_time AS time) < CAST(start_time AS time) + interval '30 minutes'
+-- AND CAST(e.beg_time AS time) > CAST(start_time AS time) - interval '30 minutes'
+
 AND   ga.spaces - o.occupied > 0
+
 GROUP BY ga.deck
 ORDER BY avail
 $$ LANGUAGE SQL STABLE STRICT;
 
-ALTER FUNCTION class_time_garage_avail(start_time text) OWNER TO parking;
+ALTER FUNCTION class_time_garage_avail(start_time time) OWNER TO parking;
