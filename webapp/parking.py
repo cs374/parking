03a4be8efe4662d9e764ca6ac2e39e7c0ef896d9
Alex Tran <anh.alex.tran@gmail.com>
2022-03-28 19:26:25 -0400

    Updated files so that they would show a message if an user used the wrong input format

--- webapp/parking.py
+++ webapp/parking.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect
-import psycopg
+import psycopg, re
 
 app = Flask(__name__)
 app.secret_key = "my_key"
@@ -13,13 +13,20 @@ def return_database():
 
 @app.route('/')
 def select_class_time():
+    is_input_wrong = None
     time_input = request.args.get("time_input", "")
     if time_input:
-        db = return_database()
-        current = db.cursor()
-        current.execute(query, (time_input,))
-        data=current.fetchall()
-        print(data)
+        if re.match('^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$', time_input): 
+            print('AAAAAA')
+            db = return_database()
+            current = db.cursor()
+            current.execute(query, (time_input,))
+            data=current.fetchall()
+            print(data)
+        else :
+            data=None
+            is_input_wrong = 1
+            return render_template("parking.html", data=data, time_input=time_input, is_input_wrong=is_input_wrong)
     else:
         data = None
-    return render_template("parking.html", data=data, time_input=time_input)
+    return render_template("parking.html", data=data, time_input=time_input, is_input_wrong=is_input_wrong)
