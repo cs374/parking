2f77e21bab929733501b387efeef62e93dda5c35
Alex Tran <anh.alex.tran@gmail.com>
2022-03-29 12:33:52 -0400

    Added comments

--- webapp/parking.py
+++ webapp/parking.py
@@ -13,20 +13,26 @@ def return_database():
 
 @app.route('/')
 def select_class_time():
+    #This variable checks if the input is the right format. If it's not the right format,
+    #it will be changed to 1. 
     is_input_wrong = None
     time_input = request.args.get("time_input", "")
     if time_input:
+        #This regular expression is used for checking the format of the input. 
+        #If the input is correct, we can execute the query. 
         if re.match('^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$', time_input): 
-            print('AAAAAA')
             db = return_database()
             current = db.cursor()
             current.execute(query, (time_input,))
             data=current.fetchall()
-            print(data)
+        #If the input is the wrong format, render the template without executing the query.
+        #Also, now that is_input_wrong is 1, the rendered template will know that it needs
+        #to display an error message. 
         else :
             data=None
             is_input_wrong = 1
             return render_template("parking.html", data=data, time_input=time_input, is_input_wrong=is_input_wrong)
+    #There's no input, so nothing happens!
     else:
         data = None
     return render_template("parking.html", data=data, time_input=time_input, is_input_wrong=is_input_wrong)
