985c99e3ea3c7503fd1093d21745e849c583045b
baileyga <baileyga@dukes.jmu.edu>
2022-04-01 16:22:53 -0400

    updated faculty query and html file

--- queries/faculty.sql
+++ queries/faculty.sql
@@ -1,22 +1,25 @@
 -- looks at how many spaces are occupied in faculty lots by day
 -- and shows the number of available spots in the respective faculty lot
-DROP FUNCTION IF EXISTS how_many_faculty(deck text, hour text);
+DROP FUNCTION IF EXISTS faculty(deck text, hour text, day text);
 
-CREATE FUNCTION how_many_faculty(deck text, hour text)
-RETURNS TABLE(deck text, time_stamp text, occupied_spaces integer, available_spaces integer) AS $$
+CREATE FUNCTION faculty(deck text, hour text, day text)
+RETURNS TABLE(deck text, avg_occupied integer, avg_available integer) AS $$
 
- SELECT garage.deck, o.time_stamp, o.occupied, CAST(SUM(garage.spaces - o.occupied) AS integer) AS available 
+ SELECT garage.deck,
+ 	ROUND(AVG(o.occupied))::int AS avg_occupied, 
+	ROUND(AVG(garage.spaces - o.occupied))::int AS avg_available 
  FROM garage
     JOIN occupancy AS o ON garage.zone_id = o.zone_id
-  WHERE garage.zone_id = 2
-	OR garage.zone_id = 6
-	OR garage.zone_id = 12
-	OR garage.zone_id = 27
-    AND garage.deck = $1
-    AND SUBSTRING(time_stamp, 12, 2) = $2
-GROUP BY garage.deck, o.time_stamp, o.occupied
-ORDER BY time_stamp, deck
+  WHERE (garage.zone_id = 2 
+    OR garage.zone_id = 6 
+    OR garage.zone_id = 12 
+    OR garage.zone_id = 27)
+	AND garage.deck = $1
+    AND SUBSTRING(time_stamp::text, 12, 2) = $2
+    AND SUBSTRING(time_stamp::text, 0, 11) = $3
+GROUP BY garage.deck
+ORDER BY garage.deck
 
 $$ LANGUAGE SQL STABLE STRICT;
 
-ALTER FUNCTION how_many_faculty(time_of_day timestamp) OWNER TO parking;
+ALTER FUNCTION faculty(deck text, hour text, day text) OWNER TO parking;
