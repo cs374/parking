f850d898970300835cd3e011c68a74e486fa36de
catherineBaker6 <catherineBaker6@users.noreply.github.com>
2022-04-09 11:46:22 -0400

    Updated avg_visitors_per_hour
    This function not takes in a date input.

--- queries/avg_visitors_per_hour.sql
+++ queries/avg_visitors_per_hour.sql
@@ -1,17 +1,18 @@
 /*This function returns the average number of visitor per hour for a inputted deck */
-DROP FUNCTION IF EXISTS avg_visitors_per_hour(g_name text);
+DROP FUNCTION IF EXISTS avg_visitors_per_hour(g_name text, date date);
 
-CREATE FUNCTION avg_visitors_per_hour(g_name text)
+CREATE FUNCTION avg_visitors_per_hour(g_name text, date date)
 RETURNS TABLE(date_part double precision, deck text, hour text, avg_visitors bigint) AS $$
 
 SELECT DISTINCT EXTRACT(HOUR FROM o.time_stamp), deck, 
 	   CONCAT((EXTRACT(HOUR FROM o.time_stamp))::text, ':00') AS hour,
-	   SUM(o.visitor)/COUNT(DISTINCT(o.time_stamp::DATE)) AS avg_vistiors
+	   SUM(o.visitor) AS avg_vistiors
 FROM garage AS g
 	JOIN occupancy AS o ON g.zone_id = o.zone_id
 WHERE g.deck = $1
+	AND o.time_stamp::DATE = $2
 GROUP BY deck, EXTRACT(HOUR FROM o.time_stamp)
 ORDER BY EXTRACT(HOUR FROM o.time_stamp);
 	
 $$ LANGUAGE SQL STABLE STRICT;
-ALTER FUNCTION avg_visitors_per_hour(g_name text) OWNER TO parking;
+ALTER FUNCTION avg_visitors_per_hour(g_name text, date date) OWNER TO parking;
